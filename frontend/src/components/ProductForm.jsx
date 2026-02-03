2704e8ddde9ad025038948235c332482f819dd97
elias <eliasnerylima018@gmail.com>
2026-02-03 18:34:21 -0300

    feat: adiciona tela de cadastro e executada POST no backend

--- frontend/src/components/ProductForm.jsx
+++ frontend/src/components/ProductForm.jsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 const ProductForm = () => {
+
+    const [nome, setNome] = useState()
+    const [preco, setPreco] = useState()
+
+    const cadastrar = async (e) => {
+        e.preventDefault()
+
+        const produto = {
+            nome: nome,
+            preco: Number(preco),
+        }
+
+        const res = await fetch('http://localhost:5050/produtos/add', {
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json"
+            }, 
+            body: JSON.stringify((produto))
+        }       
+        )
+
+        setNome("")
+        setPreco("")
+    }
 
     return (
         <div className="container">
             <h1>Cadastro de Produto</h1>
-            <form>
-                <input type="text" placeholder="Insira o nome do produto" /><br/>
-                <input type="number" placeholder="Insira o valor do produto"/><br/>
+            <form onSubmit={cadastrar}>
+                <input type="text" placeholder="Insira o nome do produto" value={nome} onChange={(e) => setNome(e.target.value)}/><br/>
+                <input type="number" placeholder="Insira o valor do produto" value={preco} onChange={(e) => setPreco(e.target.value)}/><br/>
                 <input type="submit" value="Cadastrar"/>
             </form>
         </div>
