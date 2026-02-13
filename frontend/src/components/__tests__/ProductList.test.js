8cd32d3a52faf749e5b6485d6dd13dc5f8ae24c8
elias <eliasnerylima018@gmail.com>
2026-02-13 16:37:45 -0300

    test(frontend): testa se função está sendo chamada quando clica no item da lista

--- frontend/src/components/__tests__/ProductList.test.js
+++ frontend/src/components/__tests__/ProductList.test.js
@@ -1,7 +1,7 @@
 jest.mock("../ProductList.css", () => ({}))
 
 import { render } from "@testing-library/react"
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import ProductList from "../ProductList"
 
 describe("Product List", () => {
@@ -23,5 +23,26 @@ describe("Product List", () => {
 
         expect(titulo).toBeInTheDocument()
 
+    })
+
+    test("Deve chamar a função produo quando clicar no item", () => {
+
+        const lista = [
+            {   
+                id: 1,
+                nome: "Tablet",
+                preco: "800"
+            }
+        ]
+
+        const produto = jest.fn()
+
+        render(<ProductList lista={lista} produto={produto}/>)
+        
+        const item = screen.getAllByRole("listitem")
+
+        fireEvent.click(item[0])
+
+        expect(produto).toHaveBeenCalledWith(lista[0])
     })
 }) 
