6be6c9e886bc368e6316a270a4878d7654e0e289
elias <eliasnerylima018@gmail.com>
2026-02-13 15:02:21 -0300

    test(frontend): testa botão de submit

--- frontend/src/components/__tests__/ProductForm.test.js
+++ frontend/src/components/__tests__/ProductForm.test.js
@@ -1,6 +1,6 @@
-jest.mock('../ProductForm.css',() => {({})})
+jest.mock('../ProductForm.css',() => (({})))
 
-import {fireEvent, render, screen} from "@testing-library/react"
+import {fireEvent, render, screen, waitFor} from "@testing-library/react"
 import "@testing-library/jest-dom"
 import "@testing-library/user-event"
 import ProductForm from "../ProductForm"
@@ -28,5 +28,36 @@ describe("ProductForm", () => {
         expect(nome).toHaveValue('Computador')
         expect(preco).toHaveValue(1200)
 
+    })
+
+    test("Deve executar a função de submit", async () => {
+
+        const mockOnProduto = jest.fn()
+
+        global.fetch = jest.fn(() => 
+            Promise.resolve({
+                 json: () => Promise.resolve({})
+            })
+        )
+
+        render(<ProductForm onProduto={mockOnProduto} />)
+
+        const nome = screen.getByPlaceholderText("Insira o nome do produto")
+        const preco = screen.getByPlaceholderText("Insira o valor do produto")
+        const btn = screen.getByRole("button", {name: /Cadastrar/i})
+
+        fireEvent.change(nome, {target: {value: 'Computador'}})
+        fireEvent.change(preco, {target: {value: '15000'}})
+        fireEvent.click(btn)
+
+        await waitFor(() => {
+            expect(mockOnProduto).toHaveBeenCalled()
+        })
+        
+        expect(global.fetch).toHaveBeenCalled()
+
+        expect(nome).toHaveValue("")
+        expect(preco).toHaveValue(null)
+        
     })
 }) 
